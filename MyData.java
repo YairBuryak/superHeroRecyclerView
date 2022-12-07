2097c1190cbb474843a6037338085e303c7099a6
YairBuryak <104583338+YairBuryak@users.noreply.github.com>
2022-12-07 20:57:41 +0200

    Add files via upload

---
+++ MyData.java
@@ -0,0 +1,23 @@
+package com.example.superhero;
+
+
+
+public class MyData {
+
+    static String[] nameArray = {"Super-Man", "Bat-Man", "Spider-Man", "Iron-Man", "Punisher", "Caption America","Wonder-Women","Groot","Wolverine"};
+    static String[] descriptionArray = {"DC", "DC", "MARVEL", "MARVEL", "MARVEL", "MARVEL","DC","MARVEL","DC"};
+    static String[] fullDescriptionArray = {"Superman is the archetype of the superhero: he wears an outlandish costume, uses a codename, and fights evil with the aid of extraordinary abilities.\n",
+                                            "Batman's origin story features him swearing vengeance against criminals after witnessing the murder of his parents Thomas and Martha as a child, a vendetta tempered with the ideal of justice.\n",
+                                            "Spider-Man gets superhuman spider-powers and abilities from a bite from a radioactive spider; these include clinging to surfaces and ceilings, superhuman strength, speed, and agility, and detecting danger with his precognition ability called 'spider-sense'.\n",
+                                            "Tony Stark suffers a severe chest injury during a kidnapping. When his captors attempt to force him to build a weapon of mass destruction, he instead creates a mechanized suit of armor to save his life and escape captivity.\n",
+                                            "The Punisher Frank Castle,is an antihero appearing in American comic books published by Marvel Comics.\n",
+                                            "Recipient of the Super Soldier serum, World War II hero Steve Rogers fights for American ideals as one of the world’s mightiest heroes and the leader of the Avengers.\n",
+                                            "Beautiful as Aphrodite, wise as Athena, swifter than Hermes, and stronger than Hercules, Princess Diana of Themyscira fights for peace in Man's World.\n",
+                                             "This tree-like being branched out of his comfort zone to help the Guardians of the Galaxy keep the people of the universe safe.\n",
+                                            "It was revealed that Wolverine's mutant powers included superhuman strength and reflexes, enhanced senses and tracking abilities, and a special healing power that also slows his aging.\n"};
+
+    static Integer[] drawableArray = {R.drawable.s, R.drawable.b, R.drawable.sp,
+            R.drawable.i, R.drawable.p, R.drawable.c,R.drawable.wo,R.drawable.g,R.drawable.w};
+
+    static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6 , 7, 8};
+}
